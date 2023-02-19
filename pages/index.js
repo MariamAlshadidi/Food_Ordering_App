4a9f848ad17f0206e7faa440f28890e439b96c8b
MariamAlshadidi <mariamshadidi@hotmail.com>
2023-02-19 21:35:24 +0300

    Carousel Slider Home page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Featured from '@/components/Featured'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    homepage
+      <Featured />
  
     </>
   )
