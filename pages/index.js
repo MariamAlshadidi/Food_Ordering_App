e8677fbd19fbd06724568cdbd15cfbfff2fc0a26
MariamAlshadidi <mariamshadidi@hotmail.com>
2023-03-02 21:37:17 +0300

    Product page by Id

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Featured from '@/components/Featured'
+import Head from "next/head";
+import Image from "next/image";
+import Featured from "../components/Featured";
 import ProductList from '@/components/ProductList'
-
-const inter = Inter({ subsets: ['latin'] })
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
-        <title>Order Resturant App</title>
-        <meta name="description" content="Best Pizza KSA" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Pizza Restaurant in Newyork</title>
+        <meta name="description" content="Best pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Featured />
-      <ProductList />
- 
-    </>
-  )
+      <Featured/>
+      <ProductList/>
+    </div>
+  );
 }
